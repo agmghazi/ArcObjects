9371d8b483e15f2063aa68f3d54978a4c5f5b778
Ahmed Gamal <agmghazi@hotmail.com>
2020-10-27 10:04:20 +0300

    get all layers

--- TestButton/testButton.cs
+++ TestButton/testButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
+using ESRI.ArcGIS.ArcMapUI;
 
 namespace TestButton
 {
@@ -14,7 +15,14 @@ namespace TestButton
 
         protected override void OnClick()
         {
-            MessageBox.Show("test button");
+            IMxDocument pMxDocument = (IMxDocument)ArcMap.Application.Document;
+            string layers = "";
+
+            for (int i = 0; i < pMxDocument.FocusMap.LayerCount; i++)
+            {
+                layers = layers + pMxDocument.FocusMap.Layer[i].Name + Environment.NewLine;
+            }
+            MessageBox.Show("Hello, world "+ Environment.NewLine+layers);
         }
         protected override void OnUpdate()
         {
