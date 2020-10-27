5e7df5a3f390cb3a1c0ede0538a518f0191103c4
Ahmed Gamal <agmghazi@hotmail.com>
2020-10-27 22:54:19 +0300

    WorkWithDomains

---
+++ WorkWithValueDomain/WorkWithValueDomain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesGDB;
+using System.Windows.Forms;
+
+namespace WorkWithValueDomain
+{
+    public class WorkWithValueDomain : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public WorkWithValueDomain()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            IWorkspaceFactory pWorkspaceFactory = new FileGDBWorkspaceFactory();
+
+            IWorkspace pWorkspace = pWorkspaceFactory.OpenFromFile(@"D:\Applications\ArcObjects\TestButton\GeoDP\GeoDB.gdb", ArcMap.Application.hWnd);
+
+            IWorkspaceDomains pWorkspaceDomains = pWorkspace as IWorkspaceDomains;
+            ICodedValueDomain pDomain = pWorkspaceDomains.DomainByName["Resturants"] as ICodedValueDomain;
+
+            for (int i = 0; i < pDomain.CodeCount; i++)
+            {
+                MessageBox.Show("Code: " + pDomain.Value[i] + Environment.NewLine + "Description: " + pDomain.Name[i]);
+
+            }
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
