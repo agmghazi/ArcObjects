92131aa5b4ac892b47e0a8822f20905c6190fabc
Ahmed Gamal <agmghazi@hotmail.com>
2020-10-27 09:49:54 +0300

    test button

---
+++ TestButton/testButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TestButton
+{
+    public class testButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public testButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            MessageBox.Show("test button");
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
