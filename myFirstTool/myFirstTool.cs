de42adb35365605b7129a27558a14b09ddec65b3
Ahmed Gamal <agmghazi@hotmail.com>
2020-10-27 10:37:21 +0300

    get map cordinates

---
+++ myFirstTool/myFirstTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Desktop.AddIns;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Geometry;
+
+namespace myFirstTool
+{
+    public class myFirstTool : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public myFirstTool()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+        protected override void OnMouseUp(MouseEventArgs arg)
+        {
+            MessageBox.Show("Mouse X: " + arg.X + Environment.NewLine + "Mouse Y: " + arg.Y);
+
+
+            IMxDocument pXmDoc = (IMxDocument)ArcMap.Application.Document;
+
+            IPoint pPoint = pXmDoc.ActivatedView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
+
+            MessageBox.Show("Map X: " + pPoint.X + Environment.NewLine + "Map Y: " + pPoint.Y + Environment.NewLine + "Map Z: " + pPoint.Z);
+        }
+    }
+
+}
