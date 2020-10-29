e61edeb4331756e13ffb25d2a67c7a354908531d
Ahmed Gamal <agmghazi@hotmail.com>
2020-10-29 11:13:39 +0300

    create rows

---
+++ createRowInTable/createRowInTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace createRowInTable
+{
+    public class createRowInTable : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public createRowInTable()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            IMxDocument pMDoc = ArcMap.Application.Document as IMxDocument;
+            IFeatureLayer pFlayer = pMDoc.ActiveView.FocusMap.Layer[0] as IFeatureLayer;
+            IDataset pDs = pFlayer.FeatureClass as IDataset;
+
+            IFeatureWorkspace pFWs = pDs.Workspace as IFeatureWorkspace;
+            ITable pTable = pFWs.OpenTable("Tables");
+
+            IWorkspaceEdit pWsE = pFWs as IWorkspaceEdit;
+            pWsE.StartEditing(true);
+            pWsE.StartEditOperation();
+
+            IRow pRow = pTable.CreateRow();
+            pRow.Value[pRow.Fields.FindField("Name")] = "Ahmed Gamal";
+            pRow.Value[pRow.Fields.FindField("Rank")] = 5;
+            pRow.Store();
+
+
+            pWsE.StopEditOperation();
+            pWsE.StopEditing(true);
+
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
